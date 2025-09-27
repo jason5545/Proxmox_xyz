999071e0ddaba732d26e0d294d61953e6be28c0f
Jui-Chen Chien <jason5545@gmail.com>
2025-09-27 19:09:36 +0800

    Set up Vite site for GitHub Pages deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ReportSite from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ReportSite />
+  </React.StrictMode>
+);
